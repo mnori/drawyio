4e363ed5c98fb19405200bbe08b898c2e7066dd5
matthew <matthew.gs.norris@gmail.com>
2017-05-19 23:37:19 +0100

    Some progress with the test

--- public/misc/conversion.js
+++ public/misc/conversion.js
@@ -1,3 +1,13 @@
-function initConversion() {
-	console.log("initConversion()");
+function run() {
+	console.log("run()");
+	// var canvas = self.renderer.extract.canvas(self.stagingContainer);
+	self.renderer = PIXI.autoDetectRenderer(200, 200, {
+		"antialias": true,
+		"transparent": true,
+		"clearBeforeRender": false,
+		"preserveDrawingBuffer": true
+	});
+	var view = $(self.renderer.view);
+	$("body").append(view);
+	view.addClass("source_canvas")
 }
