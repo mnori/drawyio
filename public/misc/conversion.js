833dfa8addabd4ca8143e561b4401f5e22aa1185
matthew <matthew.gs.norris@gmail.com>
2017-05-20 12:05:05 +0100

    Render circle

--- public/misc/conversion.js
+++ public/misc/conversion.js
@@ -1,7 +1,16 @@
-function run() {
-	console.log("run()");
-	// var canvas = self.renderer.extract.canvas(self.stagingContainer);
-	self.renderer = PIXI.autoDetectRenderer(200, 200, {
+var renderer, container, graphics;
+
+function main() {
+	console.log("main()");
+
+	// Render the circle
+	setup();
+	renderCircle();
+}
+
+function setup() {
+	// Create the source canvas to render a circle onto
+	renderer = PIXI.autoDetectRenderer(200, 200, {
 		"antialias": true,
 		"transparent": true,
 		"clearBeforeRender": false,
@@ -9,5 +18,18 @@ function run() {
 	});
 	var view = $(self.renderer.view);
 	$("body").append(view);
-	view.addClass("source_canvas")
+	view.addClass("source_canvas");
+
+	container = new PIXI.Container();
+	graphics = new PIXI.Graphics();
+
+	container.addChild(graphics);
+}
+
+function renderCircle() {
+	graphics.beginFill(0xff0000dd, 1);
+	graphics.lineStyle(0);
+	graphics.drawCircle(100, 100, 50);
+	graphics.endFill();
+	renderer.render(container);
 }
