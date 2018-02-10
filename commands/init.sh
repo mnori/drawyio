ad1f8ef9230178dbd02eb6eab6de7d1fe314de54
Matthew Norris <mnori@github.com>
2018-02-10 12:43:10 +0000

    init.sh now does every setup step. attempts to create and run at the end

--- commands/init.sh
+++ commands/init.sh
@@ -1,5 +1,10 @@
+# Builds everything from scratch and runs the project.
+
 ./cleanup.sh
 docker image prune -a
 cd ..
 docker build -t mnori/drawcloud .
+cd commands
+./create-mysql.sh
+./create.sh
 
